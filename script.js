240948e9405c761f3db059fdcca5a38524652a0a
dtolhuijs <deisi.bonilla@gmail.com>
2016-06-20 21:53:37 +0200

    Bind done toggle to checkbox onchage event

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// fix total-count
-$("#total-count").html($(".todo").length);
-
 function updateCounters() {
   var ntodos = $(".todo").length;
   var ncompleted = $(".completed").length
@@ -16,6 +13,8 @@ function toggleDone() {
   var checkbox = this;
 
   $(checkbox).parent().toggleClass("completed");
+
+  $("input[type=checkbox]").bind('change', toggleDone);
 
   updateCounters();
 }
